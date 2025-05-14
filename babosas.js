4c83a3abe29a683a43634d2bd2a5df1feb36f4c1
Reaper181222 <reaperfichasotros@gmail.com>
2025-05-13 23:56:56 -0300

    Update babosas.js

--- babosas.js
+++ babosas.js
@@ -1,29 +1,29 @@
-{
-    "AquaBeek": {
-        "Ataque": 70,
-        "Defensa": 50,
-        "Velocidad": 90,
-        "Energía Elemental": 55,
-        "Precisión": 60,
-        "Control": 85,
-        "Elemento": "Agua"
-    },
-    "Aracnired": {
-        "Ataque": 40,
-        "Defensa": 30,
-        "Velocidad": 85,
-        "Energía Elemental": 75,
-        "Precisión": 80,
-        "Control": 90,
-        "Elemento": "Tierra"
-    },
-    "Arenosa": {
-        "Ataque": 75,
-        "Defensa": 80,
-        "Velocidad": 90,
-        "Energía Elemental": 60,
-        "Precisión": 70,
-        "Control": 80,
-        "Elemento": "Tierra"
-    }
-}
+const datosBabosas = {
+  "AquaBeek": {
+    "Ataque": 70,
+    "Defensa": 50,
+    "Velocidad": 90,
+    "Energía Elemental": 55,
+    "Precisión": 60,
+    "Control": 85,
+    "Elemento": "Agua"
+  },
+  "Aracnired": {
+    "Ataque": 40,
+    "Defensa": 30,
+    "Velocidad": 85,
+    "Energía Elemental": 75,
+    "Precisión": 80,
+    "Control": 90,
+    "Elemento": "Tierra"
+  },
+  "Arenosa": {
+    "Ataque": 75,
+    "Defensa": 80,
+    "Velocidad": 90,
+    "Energía Elemental": 60,
+    "Precisión": 70,
+    "Control": 80,
+    "Elemento": "Tierra"
+  }
+};
