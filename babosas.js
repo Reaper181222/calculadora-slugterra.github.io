240c9ecc95f429d08a1797096348a0e34220c9f7
Reaper181222 <reaperfichasotros@gmail.com>
2025-05-15 14:18:44 -0300

    fotos agregadas

--- babosas.js
+++ babosas.js
@@ -6,7 +6,9 @@ const datosBabosas = {
     "Energía Elemental": 55,
     "Precisión": 60,
     "Control": 85,
-    "Elemento": "Agua"
+    "Elemento": "Agua",
+    "Foto Normal": "https://static.wikia.nocookie.net/bajoterra-info/images/4/43/AquaBeeks_1.png/revision/latest/scale-to-width-down/250?cb=20130806174154&path-prefix=es",
+    "Foto Malvada": "https://static.wikia.nocookie.net/bajoterra-slug/images/c/ca/Aquafreak_Protoform.png/revision/latest?cb=20131225184921&path-prefix=es"
   },
   "Aracnired": {
     "Ataque": 40,
@@ -15,7 +17,9 @@ const datosBabosas = {
     "Energía Elemental": 75,
     "Precisión": 80,
     "Control": 90,
-    "Elemento": "Tierra"
+    "Elemento": "Tierra",
+    "Foto Normal": "https://static.wikia.nocookie.net/bajoterra-slug/images/4/45/Babosa_aracniredes_.png/revision/latest/scale-to-width/360?cb=20131107233319&path-prefix=es",
+    "Foto Malvada": null
   },
   "Arenosa": {
     "Ataque": 75,
@@ -24,6 +28,8 @@ const datosBabosas = {
     "Energía Elemental": 60,
     "Precisión": 70,
     "Control": 80,
-    "Elemento": "Tierra"
+    "Elemento": "Tierra",
+    "Foto Normal": null,
+    "Foto Malvada": null
   }
 };
