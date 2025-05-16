f38011e8366ee8e4ca157d9320677f6b24641a05
Reaper181222 <reaperfichasotros@gmail.com>
2025-05-16 01:03:00 -0300

    const img = document.getElementById(`foto${num}`);   const especieLower = especie.toLowerCase();   const tipo = malvada ? "malvada" : "normal";   img.src = `Babosas/${especieLower}_${tipo}.png`;

--- babosas.js
+++ babosas.js
@@ -6,9 +6,7 @@ const datosBabosas = {
     "Energía Elemental": 55,
     "Precisión": 60,
     "Control": 85,
-    "Elemento": "Agua",
-    "Foto Normal": "/Babosas/aquabeek_normal.png",
-    "Foto Malvada": "/Babosas/aquabeek_malvada.png"
+    "Elemento": "Agua"
   },
   "Aracnired": {
     "Ataque": 40,
@@ -17,9 +15,7 @@ const datosBabosas = {
     "Energía Elemental": 75,
     "Precisión": 80,
     "Control": 90,
-    "Elemento": "Tierra",
-    "Foto Normal": "https://static.wikia.nocookie.net/bajoterra-slug/images/4/45/Babosa_aracniredes_.png/revision/latest/scale-to-width/360?cb=20131107233319&path-prefix=es",
-    "Foto Malvada": "https://static.wikia.nocookie.net/bajoterra-slug/images/d/d2/Descarga_attaknet.jpg/revision/latest?cb=20140428224852&path-prefix=es"
+    "Elemento": "Tierra"
   },
   "Arenosa": {
     "Ataque": 75,
@@ -28,8 +24,6 @@ const datosBabosas = {
     "Energía Elemental": 60,
     "Precisión": 70,
     "Control": 80,
-    "Elemento": "Tierra",
-    "Foto Normal": "https://static.wikia.nocookie.net/bajoterra-slug/images/2/2d/Babosa_arenoza.png/revision/latest/scale-to-width/360?cb=20131223151406&path-prefix=es",
-    "Foto Malvada": "https://static.wikia.nocookie.net/bajoterra-slug/images/9/92/Sand-mangler1.png/revision/latest/scale-to-width/360?cb=20250423061805&path-prefix=es"
+    "Elemento": "Tierra"
   }
 };
